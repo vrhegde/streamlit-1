244f499b98c0271e44b61c81c4bff2b91cae27d9
vrhegde <62508392+vrhegde@users.noreply.github.com>
2022-01-08 19:48:44 -0500

    Update exmschl.py

--- exmschl.py
+++ exmschl.py
@@ -7,36 +7,39 @@ import matplotlib.pyplot as plt
 
 st.title('Boston Exam School Admissions')
 """
-    #### The effect of uneven tier distrubution and bonus points on probablity of amdission into exam schools.
+    #### The effect of uneven tier distribution and bonus points on probability of admission into exam schools.
 
-    Until 2020, admission to Bostons's three 'exam schools' was determined by using a combination of a standardized test (the ISEE) and the student's grades. Each of these two criteria was weighed equally. Students competed for these seats in a single combined pool. Thus merit, compared on a city-wide basis, was the only metric for admission into these selective schools.
+    Until 2020, admission to Boston's three 'exam schools' was determined by a combination of a standardized test (the ISEE) and the grades. Each of these two criteria was weighted equally. Students competed for these seats in a single combined pool. Thus a city-wide competetion based on merit, was the only metric for admission into these selective schools.
 
-    The new admission policy for these schools has made several changes:
-     1. The seats are divided into 8 socio-economic tiers, determined by the census-tract their home is located in. Students can compete only within the assigned tier, and not across different tiers.
-     2. ISEE is eliminated.
-     3. Grades are weighed 70% and a new test, the NWEA MAP test, is weighted 30%.
+    The new admission policy for these schools introduces several changes:
+     1. Invitations are divided across 8 socio-economic tiers made up of census-tracts with similar socio-economic profile. That profile is based on 
+     the following five factors: Percentage of persons below powerty, percentage of households not occupied by the owner, percentage of families headed by a single parent, 
+     percentage of households where English is not the primary language spoken, and educational attainment. Tiers are sized to include an equal number of fifth-eighth grade students.
+     Students are assigned to tiers based on the census-tract their home is located in. Students compete only within their assigned tier, and not across different tiers.
+     2. The ISEE is eliminated.
+     3. Grades are weighted at 70% and a new test, the NWEA MAP test, is weighted at 30%.
      4. The MAP test will not be considered for the 2021-22 admission cycle.
 
-    There are 2 features of this plan, in its current state, that will unfairly disadvantage many students. First, the tiers are divided according to the estimated number of school aged children within a specific age range, and not by the number of eligible applicants (eligible applicants are students with grade average of B or higher). As the number of eligible students is substantially higher in the upper tiers, those students will have a lower probablity of getting selected. Second, every student going to a schools where more than 40% of students are socio-economically challanged will get 10 bonus points (out of a total score of 100) added on to their final score used to determine exam school admissions. These bonus points are determined by the school, and not be the socio-economic situation of the student. As about 80% of BPS students attend bonus-points schools, the remaining 20% of students will be severly disadvantaged.
+    There are 2 features of this plan, that will unfairly disadvantage many students. First, the tiers are divided according to the estimated number of school aged children within a specific age range, and not by the number of eligible applicants (eligible applicants are students with a GPA of B or higher). Because the number of eligible students is substantially higher in the upper tiers, students in these tiers will have a lower probability of earning an invitation. Second, every student going to a schools with more than 40% economically disadvantaged students will get 10 bonus points (out of a total score of 100) added on to their final score. These bonus points are determined by the school, and not by the socio-economic situation of the student. Since 80% of BPS students attend bonus-points schools, most students will recieve the bonus points. Only 20% will not. 
 
     The above factors, combined with the fact that the MAP test will not be used for this 2021-2022 admission cycle, ensures that many A+ students, particularly in West-Roxbury (where most non-bonus-point-schools are located) will have no chance of being admitted to these selective schools.
 
-    The aim of this project is to demonstrate the effect of bonus points, and of using unevenly distributed tiers, on probablity of admission to selective schools. In this project, we will generate synthetic data (within the bounds of what we know from previous admission cycles) and run repeated simulations to determine the probablity distribution of admissions to these schools.
+    The aim of this project is to demonstrate the effect of bonus points, and of using unevenly distributed tiers, on the probability of admission to selective schools. In this project, we will generate synthetic data (within the bounds of what we know from previous admission cycles) and run repeated simulations to determine the probability distribution of admissions to these schools.
 
 
     #### Method:
-    We will first create synthetic data using two different methods. The first without bonus points, and the second with bonus points. We will create two tier distributions: First tiers that have the same number of students (Even distribution), and second with more students in the upper tiers (Skewed distribution). For the skewed tier distribution, relative distribution of students per tier will be estimated by data presented by the school committee and the exam school taks force.
+    We will first create synthetic data using two different methods. The first without bonus points, and the second with bonus points. We will create two tier distributions: First, tiers that have the same number of students (Even distribution); second, with more students in the upper tiers (Skewed distribution). For the skewed tier distribution, relative distribution of students per tier will be estimated by data presented by the school committee and the exam school task force.
 
-    We will create a form where one can specify the number of applicants, the total number of seats, and the number of trial simulations to run. This will be the input interface for those who want to tinker with the data but are not familier with the workings of a python notebook.
+    We will create a form where one can specify the number of applicants, the total number of seats, and the number of trial simulations to run. This will be the input interface for those who want to tinker with the data but are not familiar with the workings of a python notebook.
 
     Synthetic data creation:
-    Two functions to create synthetic data will be written. The only difference between the functions is that one does not use bonus points in calculating the final score, and the other does. Two different methods are used to generate data as we want to explore the impact of bonus points on probablity of admission.
+    Two functions to create synthetic data will be written. The only difference between the functions is that one does not use bonus points in calculating the final score, and the other does. Two different methods are used to generate data as we want to explore the impact of bonus points on the probability of admission.
 
     1. A DataFrame containing n (= number of student) rows will be created.
-    2. Raw scores will be randomly generated and assigned to each student. Scores will range from 7 to 12 (grades B to A +).
+    2. Raw scores will be randomly generated and assigned to each student. Scores will range from 8 to 12 (grades B to A +).
     3. Scaled scores will be calculated by multiplying raw score by 100/12. This was the method previously used be BPS, and was employed when they ran simulations for the exam school task force.
-    4. Weather or not the student gets bonus points will begenerated randomly (yes =1, no =0). Random binary generator will be set such that 80% of the times the number 1 is returned (thus 80% of students get bonus points).
-    5. Final scores are calculated by adding 10 points to scaled score if bonus points method used. Alternately, final score will be the same as the scaled score, if non-bonus point method is used.
+    4. Whether or not the student gets bonus points will be generated randomly (yes =1, no =0). The random binary generator will be set such that 80% of the times the number 1 is returned (thus 80% of students get bonus points).
+    5. Final scores are calculated by adding 10 points to the scaled score if the bonus points method is used. Alternately, if the non-bonus point method is used, the final score will be the same as the scaled score.
 
     Repeated simulations:
     The number of simulations to be run will be specified in the input form. (Running 1000 simulations is recommended, but that will take a few minutes. If you want quick answers, run a 100 simulations).
@@ -94,7 +97,7 @@ def make_data1(tier_prob_dist):
   keys_col = np.arange(0,n_students,1)  #student identifier from 0-2500
   tier_col = np.random.choice(np.arange(1, 9),size = [n_students,1], p = tier_prob_dist).flatten().tolist() # generate a list to poppulate the tier column
   bonus_col = np.random.choice(a=[1,0], size=(n_students,1), p=[.8,.2]).flatten().tolist() # generate a list to poppulate the tier column
-  raw_score = np.random.randint(7, 13, [n_students,1]).flatten().tolist()
+  raw_score = np.random.randint(8, 13, [n_students,1]).flatten().tolist()
 
   sim_df = pd.DataFrame(zip(keys_col,tier_col,bonus_col,raw_score), columns = ['keys','tier','bonus','raw_score'])
   sim_df['scaled_score'] = sim_df.raw_score * (100/12)
